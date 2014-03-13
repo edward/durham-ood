40ef4b942d578eb3b4a5721ccc73b693bae80bc8
Katrina Owen <katrina.owen@gmail.com>
2014-03-13 10:33:28 -0400

    Add extract verse step

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,15 +8,86 @@ class Bottles
   end
 
   def verse(number)
+    Verse.new(number).to_s
+  end
+end
+
+class Verse
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    "#{current_inventory} #{current_container} #{liquid} #{location}, ".capitalize +
+    "#{current_inventory} #{current_container} #{liquid}.\n" +
+    "#{action}, " +
+    "#{next_inventory} #{next_container} #{liquid} #{location}.\n"
+  end
+
+  private
+
+  def liquid
+    'of beer'
+  end
+
+  def location
+    'on the wall'
+  end
+
+  def current_inventory
     case number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      'no more'
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      number
+    end
+  end
+
+  def next_inventory
+    case number
+    when 0
+      99
+    when 1
+      'no more'
+    else
+      number - 1
+    end
+  end
+
+  def current_container
+    case number
+    when 1
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def next_container
+    case number
+    when 2
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def action
+    case number
+    when 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def pronoun
+    case number
+    when 1
+      'it'
+    else
+      'one'
     end
   end
 end
