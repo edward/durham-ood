a779de302b29ef10195e102d70a8380aba7a6e56
Luke Morris <luke@imfuna.com>
2014-03-13 23:13:24 -0400

    Refinement of idea.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,24 +1,24 @@
 class HouseSong
   def sing
-    (0..11).map{|number| verse(number)}
+    (1..12).map{|number| verse(number)}
   end
 
   def verse(number)
-    'This is ' + SUBJECTS.take(number+1).reverse.join(' ') + ".\n\n"
+    'This is ' + SUBJECTS.last(number).join(' ') + ".\n\n"
   end
 end
 
 SUBJECTS = [
-  'the house that Jack built',
-  'the malt that lay in',
-  'the rat that ate',
-  'the cat that killed',
-  'the dog that worried',
-  'the cow with the crumpled horn that tossed',
-  'the maiden all forlorn that milked',
-  'the man all tattered and torn that kissed',
-  'the priest all shaven and shorn that married',
-  'the rooster that crowed in the morn that woke',
+  'the horse and the hound and the horn that belonged to',
   'the farmer sowing his corn that kept',
-  'the horse and the hound and the horn that belonged to'
+  'the rooster that crowed in the morn that woke',
+  'the priest all shaven and shorn that married',
+  'the man all tattered and torn that kissed',
+  'the maiden all forlorn that milked',
+  'the cow with the crumpled horn that tossed',
+  'the dog that worried',
+  'the cat that killed',
+  'the rat that ate',
+  'the malt that lay in',
+  'the house that Jack built'
 ]
