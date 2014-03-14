bc1b89b3c599eb78c48ca3861fe12e4d68d73ad3
Luke Morris <luke@imfuna.com>
2014-03-13 23:35:29 -0400

    Derive from colors array

--- checkerboard/lib/checkerboard.rb
+++ checkerboard/lib/checkerboard.rb
@@ -1,9 +1,15 @@
 class Checkerboard
-  def initialize(side)
+  attr_reader :size
+  def initialize(size)
+    @size = size
   end
 
   def to_s
-    "B W\n" +
-    "W B\n"
+    "#{colors[0]} #{colors[1]}\n" +
+    "#{colors[1]} #{colors[0]}\n"
+  end
+
+  def colors
+    %w[ B W ]
   end
 end
