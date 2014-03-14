10b7c89c6a44f0f849fbc00b39618a37dff0ed01
Luke Morris <luke@imfuna.com>
2014-03-13 23:05:16 -0400

    Change to recursive approach

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,32 +1,53 @@
 class HouseSong
   def sing
-    (1..12).map{|number| verse(number)}
+    (0..11).map{|number| verse(number)}
   end
 
   def verse(number)
-    'This is' + VerseSubject.for_verse(number) + ".\n\n"
+    'This is' + Subject.for_verse(number) + ".\n\n"
   end
 end
 
-class VerseSubject
+class Subject
   def self.for_verse(number)
-    all_subjects.take(number).inject('') { |result, subject| ' ' + subject + result }
+    Subject.new(number).to_s
   end
 
-  def self.all_subjects
-    [
-      'the house that Jack built',
-      'the malt that lay in',
-      'the rat that ate',
-      'the cat that killed',
-      'the dog that worried',
-      'the cow with the crumpled horn that tossed',
-      'the maiden all forlorn that milked',
-      'the man all tattered and torn that kissed',
-      'the priest all shaven and shorn that married',
-      'the rooster that crowed in the morn that woke',
-      'the farmer sowing his corn that kept',
-      'the horse and the hound and the horn that belonged to'
-    ]
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    ' ' + SUBJECTS[number] + next_subject.to_s
+  end
+
+  def next_subject
+    if number == 0
+      NullSubject.new
+    else
+      Subject.new(number-1)
+    end
   end
 end
+
+class NullSubject
+  def to_s
+    ''
+  end
+end
+
+SUBJECTS = [
+  'the house that Jack built',
+  'the malt that lay in',
+  'the rat that ate',
+  'the cat that killed',
+  'the dog that worried',
+  'the cow with the crumpled horn that tossed',
+  'the maiden all forlorn that milked',
+  'the man all tattered and torn that kissed',
+  'the priest all shaven and shorn that married',
+  'the rooster that crowed in the morn that woke',
+  'the farmer sowing his corn that kept',
+  'the horse and the hound and the horn that belonged to'
+]
