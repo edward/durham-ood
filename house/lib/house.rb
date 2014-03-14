26b9845c5d921419b7729d1703ebee740d6e34c6
Luke Morris <luke@imfuna.com>
2014-03-14 11:29:55 -0400

    Dependency injection on top of simple.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,24 +1,14 @@
 class HouseSong
-  def self.sing
-    (1..12).map{|number| verse(number)}
+  attr_reader :subjects
+  def initialize(subjects)
+    @subjects = subjects
   end
 
-  def self.verse(number)
-    'This is ' + SUBJECTS.last(number).join(' ') + ".\n\n"
+  def sing
+    subjects.length.times.map{|number| verse(number+1)}
+  end
+
+  def verse(number)
+    'This is ' + subjects.last(number).join(' ') + ".\n\n"
   end
 end
-
-SUBJECTS = [
-  'the horse and the hound and the horn that belonged to',
-  'the farmer sowing his corn that kept',
-  'the rooster that crowed in the morn that woke',
-  'the priest all shaven and shorn that married',
-  'the man all tattered and torn that kissed',
-  'the maiden all forlorn that milked',
-  'the cow with the crumpled horn that tossed',
-  'the dog that worried',
-  'the cat that killed',
-  'the rat that ate',
-  'the malt that lay in',
-  'the house that Jack built'
-]
