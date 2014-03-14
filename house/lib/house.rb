bcee79c0da447c46299fbef5704e393028a0e19a
Luke Morris <luke@imfuna.com>
2014-03-13 22:15:48 -0400

    Simplify into verse_subjects

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,22 +1,30 @@
 class HouseSong
   def sing
-    verses.map{|v| 'This is ' + v + "\n\n"}
+    verses.map{|v| v + "\n\n"}
   end
 
   def verses
+    (0..11).map{|number| verse(number)}
+  end
+
+  def verse(number)
+    'This is' + verse_subjects[0..number].inject('') { |result, subject| ' ' + subject + result } + '.'
+  end
+
+  def verse_subjects
     [
-      'the house that Jack built.',
-      'the malt that lay in the house that Jack built.',
-      'the rat that ate the malt that lay in the house that Jack built.',
-      'the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.',
-      'the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.'
+      'the house that Jack built',
+      'the malt that lay in',
+      'the rat that ate',
+      'the cat that killed',
+      'the dog that worried',
+      'the cow with the crumpled horn that tossed',
+      'the maiden all forlorn that milked',
+      'the man all tattered and torn that kissed',
+      'the priest all shaven and shorn that married',
+      'the rooster that crowed in the morn that woke',
+      'the farmer sowing his corn that kept',
+      'the horse and the hound and the horn that belonged to'
     ]
   end
 end
